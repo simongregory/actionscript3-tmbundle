950b01b164d7d4ec66278f905492836850aa0777
Simon Gregory <simon@helvector.org>
2008-05-04 21:06:32 +0000

    Switching declaration snippets to tab triggered commands. This slims down the amount of items in the bundle and will allow customisation of the output - ie automatically added documentation.
    git-svn-id: http://svn.textmate.org/trunk/Review/Bundles/ActionScript 3.tmbundle@9513 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

---
+++ Support/lib/as_declarations.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby -wKU
+
+# Collection of declaration snippets for use
+# within public, protected or private namespaces.
+
+@method    = " function ${1:name}($2):${3:void}{\n\t$0${3/void$|(.+)/(?1:return null;)/}\n}"
+@getter    = " function get ${1:name}():${2:Object}{\n\treturn ${3:_$1};\n}$0"
+@setter    = " function set ${1:name}(value:${2:Object}):void {\n\t${3:_$1} = value;\n}$0"
+@variable  = " var _${1:variableName}:${2:Object};"
+@property  = " var ${1:propertyName}:${2:Object};"
+@namespace = " namespace ${1:nameSpaceName};"
+@constant  = " const ${1:CONSTANT_NAME}:${2:Object};"
+
+@static_variable = "static" + @property
+@static_function = "static" + @method
+@static_constant = "static" + @constant
+
+def get_statics()
+    r = [ 
+        list_object('static const', @static_constant),
+        list_object('static function', @static_function),
+        list_object('static var', @static_variable)
+    ]
+end
+
+def get_snippets(ns)
+    
+    v = ns == "public" ? @property : @variable
+    
+    r = [
+          { 'title' => 'function', 'data' => ns + @method}, 
+          { 'title' => 'get', 'data' => ns + @getter},      
+          { 'title' => 'set', 'data' => ns + @setter}, 
+          { 'title' => 'var', 'data' => ns + v},
+          { 'title' => 'namespace', 'data' => ns + @namespace},          
+    ]
+    
+    r += [
+        { 'title' => '----'},
+        { 'title' => 'static function', 'data' => ns + " static " + @method }, 
+        { 'title' => 'static get', 'data' => ns + " static " + @getter }, 
+        { 'title' => 'static set', 'data' => ns + " static " + @setter }, 
+        { 'title' => 'static var', 'data' => ns + " static " + v }, 
+        { 'title' => 'static const', 'data' => ns + " static " + @constant }
+    ] unless ns == "protected"
+    
+    return r
+end
+
+# Getter/Setters
+
+def getter
+    @getter
+end
+
+def setter
+    @setter
+end
+
+def method(name)
+    if name != ""
+        m = @method.sub( "${1:name}", "${1:"+name+"}")
+        return m
+    end
+    return @method    
+end
+
+def list_object(title,data)
+    return { 'title' => title, 'data' => data}
+end
