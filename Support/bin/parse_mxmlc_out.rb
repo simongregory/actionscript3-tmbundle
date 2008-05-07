b2bd81aa782bf7e46da9221cfe2bba6d87a5b4e4
Simon Gregory <simon@helvector.org>
2008-05-07 09:48:07 +0000

    Adding exception to catch any compiler output that fails regexp. The offending line is output raw.
    git-svn-id: http://svn.textmate.org/trunk/Review/Bundles/ActionScript 3.tmbundle@9528 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/bin/parse_mxmlc_out.rb
+++ Support/bin/parse_mxmlc_out.rb
@@ -24,50 +24,61 @@ last_match = ""
 ARGF.each do |line|
 
     match = error_and_warn_regex.match( line )
+    
+    begin
+        
+        unless match === nil
+            print "<br/>" if last_match != ERROR_WARN_MATCH        
+            print 'Error <a title="Click to show error." href="txmt://open?url=file://' + match[1] + 
+                  '&line='+ match[3] + 
+                  '" >' + match[5] + 
+                  '</a> at line ' + match[3] + 
+                  ' in <a title="'+match[1] + 
+                  '" href="txmt://open?url=file://' + match[1] + '">' +
+                  File.basename( match[1] ) + '</a><br/>'
+            error_count += 1
+            last_match = ERROR_WARN_MATCH
+            next
+        end
+    
+        match = config_file_regex.match( line )
+        unless match === nil
+            print "<br/>" if last_match != CONFIGURATION_MATCH
+            print 'Loading configuration file: <a title="Click to open ' + match[2] + 
+            '" href="txmt://open?url=file://' + match[2] + '" >' + File.basename( match[2] ) +'</a><br/>'
+            last_match = CONFIGURATION_MATCH
+            next
+        end
 
-    unless match === nil
-        print "<br/>" if last_match != ERROR_WARN_MATCH        
-        print 'Error <a title="Click to show error." href="txmt://open?url=file://' + match[1] + 
-              '&line='+ match[3] + 
-              '" >' + match[5] + 
-              '</a> at line ' + match[3] + 
-              ' in <a title="'+match[1] + 
-              '" href="txmt://open?url=file://' + match[1] + '">' +
-              File.basename( match[1] ) + '</a><br/>'
+        match = recompile_file_regex.match( line )
+        unless match === nil
+            print "<br/>" if last_match != RECOMPILE_REASON_MATCH
+            print 'Recompiling: <a title="Click to open ' + match[2] + 
+            '" href="txmt://open?url=file://' + match[2] + '" >' + File.basename( match[2] )+'</a><br/>'
+            last_match = RECOMPILE_REASON_MATCH
+            next
+        end
+
+        match = reason_file_regex.match(line)
+        unless match === nil
+            print "<br/>" if last_match != RECOMPILE_REASON_MATCH
+            print match[1]+'<a title="Click to open ' + match[2] + '" href="txmt://open?url=file://' + match[2] + 
+            '" >' + File.basename( match[2] )+'</a> ' + match[3] + '<br/>'
+            last_match = RECOMPILE_REASON_MATCH
+            next
+        end
+
+        if line =~ /\.swf \([0-9]/
+            puts "<br/>" +line + "<br/>"
+        end
+    
+    rescue TypeError
+        
+        puts "WARING, TextMate ActionScript 3 Bundle Error, Unable to parse mxmlc output: <br/><br/>"
+        puts line + "<br/>"
+        
         error_count += 1
-        last_match = ERROR_WARN_MATCH
-        next
-    end
-
-    match = config_file_regex.match( line )
-    unless match === nil
-        print "<br/>" if last_match != CONFIGURATION_MATCH
-        print 'Loading configuration file: <a title="Click to open ' + match[2] + 
-        '" href="txmt://open?url=file://' + match[2] + '" >' + File.basename( match[2] ) +'</a><br/>'
-        last_match = CONFIGURATION_MATCH
-        next
-    end
-
-    match = recompile_file_regex.match( line )
-    unless match === nil
-        print "<br/>" if last_match != RECOMPILE_REASON_MATCH
-        print 'Recompiling: <a title="Click to open ' + match[2] + 
-        '" href="txmt://open?url=file://' + match[2] + '" >' + File.basename( match[2] )+'</a><br/>'
-        last_match = RECOMPILE_REASON_MATCH
-        next
-    end
-
-    match = reason_file_regex.match(line)
-    unless match === nil
-        print "<br/>" if last_match != RECOMPILE_REASON_MATCH
-        print match[1]+'<a title="Click to open ' + match[2] + '" href="txmt://open?url=file://' + match[2] + 
-        '" >' + File.basename( match[2] )+'</a> ' + match[3] + '<br/>'
-        last_match = RECOMPILE_REASON_MATCH
-        next
-    end
-
-    if line =~ /\.swf \([0-9]/
-        puts "<br/>" +line + "<br/>"
+        
     end
     
 end
