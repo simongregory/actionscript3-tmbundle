b0c452516a6930acdda2c485bb3363d176b62ffb
Simon Gregory <simon@helvector.org>
2008-07-11 14:29:10 +0000

    • Added accessor for common src list. • Added comments.

--- Support/lib/package_completion.rb
+++ Support/lib/package_completion.rb
@@ -8,27 +8,38 @@ require SUPPORT + '/lib/exit_codes'
 require SUPPORT + '/lib/textmate'
 require SUPPORT + '/lib/ui'
 
-def common_src_dirs
-	# Places to search for source..
-	common_src_directories = ENV['TM_AS3_USUAL_SRC_DIRS']
-	common_src_directories = "src:lib:source:test" if common_src_directories == nil
-	common_src_dir_matches = common_src_directories.split(":")
+# Returns an colon seperated list of directory names 
+# that are commonly used as the root directory for source files.
+def common_src_dir_list
+	src_dirs = ENV['TM_AS3_USUAL_SRC_DIRS']
+	src_dirs = "src:lib:source:test" if src_dirs == nil
+	src_dirs
 end
 
+# Returns an array of directory names that are commonly used
+# as the root directory for source files.
+def common_src_dirs
+	src_dirs_matches = common_src_dir_list.split(":")
+	src_dirs_matches
+end
+
+# Finds, and where sucessful returns, the package path for the specified 
+# class (word is used as paramater here as it may be a partial class name).
+# Packages paths are resolved via doc_dictionary.xml, which contains flash, fl, 
+# and mx paths, and the current tm project (when available). 
+#
+# Where mulitple possible matches are found these are presented to the user 
+# using Textmate::UI.menu.
 def find_package(word)
 	
 	TextMate.exit_show_tool_tip("Please select a term to look up.") if word.empty?
 	
-	# Places to search for source..
-	# common_src_directories = ENV['TM_AS3_USUAL_SRC_DIRS']
-	# common_src_directories = "src:lib:source:test" if common_src_directories == nil
-	# common_src_dir_matches = common_src_directories.split(":")
 	package_paths = []
 	
 	# Collect all .as and .mxml files with a filename that contains the search
 	# term. When used outside a project this step is skipped.
 	TextMate.each_text_file do |file|
-
+		
 		if file =~ /\b#{word}\w*\.(as|mxml)$/
 	       path = file.sub( PROJECT, "" );
 	       common_src_dirs.each do |remove|
@@ -42,7 +53,7 @@ def find_package(word)
 	# Open Help dictionary and find matching lines
 	toc = IO.readlines(HELPTOC)
 	seperator = package_paths.size > 0 ? true : false;
-	toc.each do |line|		
+	toc.each do |line|
 		if line =~ /href='([a-zA-Z0-9\/]*\b#{word}\w*)\.html'/
 			if seperator
 				package_paths << "-"
@@ -67,3 +78,5 @@ def find_package(word)
 	end
 
 end
+
+	
