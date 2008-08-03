a1a04ccc1cbd0fba5abe78759b7f07830d6a1963
Simon Gregory <simon@helvector.org>
2008-08-03 17:55:50 +0000

    • Removing Open MX Sourcecode (now in the Flex bundle). • Organising menu structure. • Adding escape require to flex_env • Cleaning out old code from auto complete function.
    git-svn-id: http://svn.textmate.org/trunk/Review/Bundles/ActionScript 3.tmbundle@10358 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/lib/flex_env.rb
+++ Support/lib/flex_env.rb
@@ -3,9 +3,10 @@
 SUPPORT = "#{ENV['TM_SUPPORT_PATH']}"
 BUN_SUP = "#{ENV['TM_BUNDLE_SUPPORT']}"
 
-require SUPPORT + '/lib/ui'
+require SUPPORT + '/lib/escape'
 require SUPPORT + '/lib/exit_codes'
+require SUPPORT + '/lib/ui'
 
+require BUN_SUP + '/lib/as_completions_list'
 require BUN_SUP + '/lib/class_parser'
 require BUN_SUP + '/lib/flex_mate'
-require BUN_SUP + '/lib/as_completions_list'
