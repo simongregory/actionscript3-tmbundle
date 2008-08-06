e6522c919b2b7cd9e2774acb5cfb6a76dac6b8e7
Simon Gregory <simon@helvector.org>
2008-08-06 08:15:10 +0000

    • Switching to using relative requires so the script can run from alternative bundles.

--- Support/lib/flex_env.rb
+++ Support/lib/flex_env.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby -wKU
 
 SUPPORT = "#{ENV['TM_SUPPORT_PATH']}"
-BUN_SUP = "#{ENV['TM_BUNDLE_SUPPORT']}"
+# BUN_SUP = "#{ENV['TM_BUNDLE_SUPPORT']}/lib"
+BUN_SUP = File.dirname(__FILE__)
 
 require SUPPORT + '/lib/escape'
 require SUPPORT + '/lib/exit_codes'
 require SUPPORT + '/lib/ui'
 
-require BUN_SUP + '/lib/as_completions_list'
-require BUN_SUP + '/lib/class_parser'
-require BUN_SUP + '/lib/flex_mate'
+require BUN_SUP + '/as_completions_list'
+require BUN_SUP + '/class_parser'
+require BUN_SUP + '/flex_mate'
