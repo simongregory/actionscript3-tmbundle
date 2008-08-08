21a3e58843ece081912cad36b0d0e1ee0650d3a1
Simon Gregory <simon@helvector.org>
2008-08-08 10:07:20 +0000

    • Fixing problem with import autocompletion.

--- Support/lib/flex_env.rb
+++ Support/lib/flex_env.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby -wKU
 
 SUPPORT = "#{ENV['TM_SUPPORT_PATH']}"
-# BUN_SUP = "#{ENV['TM_BUNDLE_SUPPORT']}/lib"
 BUN_SUP = File.dirname(__FILE__)
 
 require SUPPORT + '/lib/escape'
 require SUPPORT + '/lib/exit_codes'
+require SUPPORT + '/lib/textmate' 
 require SUPPORT + '/lib/ui'
-#require SUPPORT + '/lib/textmate' 
 
 require BUN_SUP + '/as_completions_list'
 require BUN_SUP + '/class_parser'
