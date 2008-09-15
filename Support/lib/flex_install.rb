370bb8688e4047282702be9269d941d34fdacd9f
Simon Gregory <simon@helvector.org>
2008-09-15 17:10:30 +0000

    • Adding undefined to completions. • Adding comments to class parsers. • Updated flex_install with target directory selection.

--- Support/lib/flex_install.rb
+++ Support/lib/flex_install.rb
@@ -2,12 +2,21 @@
 
 # Downloads and installs flex to a default location used by TextMate.
 
-require 'open-uri'
+SUPPORT = "#{ENV['TM_SUPPORT_PATH']}"
+BUN_SUP = File.dirname(__FILE__)
 
-install_path = "/Developer/SDKs/flex_sdk_3"
+require 'open-uri'
+require SUPPORT + '/lib/ui'
+
+install_path = "/"
+sdk_path = "/Developer/SDKs"
+install_path = sdk_path if File.exists?(sdk_path)
+
+TextMate::UI.request_file(:only_directories => true, :directory => install_path)
 
 flex_uri = "http://download.macromedia.com/pub/flex/sdk/flex_sdk_3.zip"
 flex_help_uri = "http://livedocs.adobe.com/flex/3/flex3_documentation.zip"
+
 
 #open("/Users/simon/Desktop/flex_sdk_3.zip","w").write(open(flex_uri).read)
 #open("/Users/simon/Desktop/flex_help.zip","w").write(open(flex_help_uri).read)
