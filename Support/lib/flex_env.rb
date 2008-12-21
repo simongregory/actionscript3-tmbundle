d94998095a9f26cb087f9b89c55b605b9257d5aa
Simon Gregory <simon@helvector.org>
2008-12-21 21:53:57 +0000

    Updating flex_env to point to classes and modules in their new locations. Removing old declarations script. Updated commands to refer to appropriate classes, modules and includes.

--- Support/lib/flex_env.rb
+++ Support/lib/flex_env.rb
@@ -10,7 +10,17 @@ require SUPPORT + '/lib/exit_codes'
 require SUPPORT + '/lib/textmate' 
 require SUPPORT + '/lib/ui'
 
-require BUN_SUP + '/property_inspector'
-require BUN_SUP + '/as_completions_list'
-require BUN_SUP + '/class_parser'
-require BUN_SUP + '/flex_mate'
+require BUN_SUP + '/fm/flex_mate'
+require BUN_SUP + '/fm/log'
+require BUN_SUP + '/fm/sdk'
+
+require BUN_SUP + '/as3/tools/source_tools'
+
+require BUN_SUP + '/as3/parsers/class_parser'
+require BUN_SUP + '/as3/parsers/property_inspector'
+
+require BUN_SUP + '/as3/completions/completions_list'
+
+require BUN_SUP + '/as3/templates/snippet_builder'
+require BUN_SUP + '/as3/templates/snippet_provider'
+require BUN_SUP + '/as3/templates/snippet_controller'
