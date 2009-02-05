865060718da9240cc4c1c8059b1a922341ddd460
Simon Gregory <simon@helvector.org>
2009-02-05 12:21:32 +0000

    • Updated to use File.expand_path when defining support dirs.

--- Support/lib/flex_env.rb
+++ Support/lib/flex_env.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby -wKU
 
-# Used as a common require to set up the env for commands. 
+# Used as a common require to set up the environment for commands. 
 
 SUPPORT = "#{ENV['TM_SUPPORT_PATH']}"
-BUN_SUP = File.dirname(__FILE__)
+BUN_SUP = File.expand_path(File.dirname(__FILE__))
 
 require SUPPORT + '/lib/escape'
 require SUPPORT + '/lib/exit_codes'
@@ -14,13 +14,10 @@ require BUN_SUP + '/fm/flex_mate'
 require BUN_SUP + '/fm/log'
 require BUN_SUP + '/fm/sdk'
 
-require BUN_SUP + '/as3/tools/source_tools'
-
+require BUN_SUP + '/as3/completions/completions_list'
 require BUN_SUP + '/as3/parsers/class_parser'
 require BUN_SUP + '/as3/parsers/property_inspector'
-
-require BUN_SUP + '/as3/completions/completions_list'
-
 require BUN_SUP + '/as3/templates/snippet_builder'
-require BUN_SUP + '/as3/templates/snippet_provider'
 require BUN_SUP + '/as3/templates/snippet_controller'
+require BUN_SUP + '/as3/templates/snippet_provider'
+require BUN_SUP + '/as3/tools/source_tools'
