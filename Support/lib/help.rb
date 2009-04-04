4f72d4de4bd7964cebe16714e7b25e768ee2c44b
simongregory <simon@helvector.org>
2009-04-05 00:28:08 +0100

    • Help command now displays the paths found in TM_FLEX_SDK_SEARCH_PATHS.

---
+++ Support/lib/help.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby -wKU
+
+# Generates the ActionScript 3 Bundle help document. 
+# Takes the help.mdown doc and injects the list of paths searched
+# when bundle items attempt to locate the flex sdk.
+
+BUN_SUP_LIB = File.expand_path(File.dirname(__FILE__))
+
+require BUN_SUP_LIB + '/fm/sdk'
+
+paths = ""
+FlexMate::SDK.sdk_dir_arr.each{ |e| paths << "* \`#{e}\`\n" }
+
+doc = IO.read(BUN_SUP_LIB+'/../help.mdown')
+
+print doc.sub('${FLEX-PATH-LIST}', paths )
