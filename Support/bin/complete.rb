1e6ca28ce8af714d6a0eea6490dccf37ef92d20e
simongregory <simon@helvector.org>
2009-05-25 12:27:28 +0100

    • Build command moved to bin from embedded tmCommand.
    This should avoid path problems where a pristine copy of the bundle is available.

---
+++ Support/bin/complete.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require ENV['TM_BUNDLE_SUPPORT'] + '/lib/flex_env'
+
+FlexMate.complete_by_scope if ENV['TM_SCOPE'] =~ /support\.function\.(top-level|flash|fl|mx|global)\.actionscript\.3/
+
+FlexMate.opt_in_to_completions
+
+p = PropertyInspector.capture
+TextMate.exit_show_tool_tip("No property found.") if p[:ref] == nil
+
+c = ClassParser.new
+
+if p[:is_static]
+	c.load_statics(STDIN.read.strip,p[:ref])
+else
+	c.load(STDIN.read.strip,p[:ref])
+end
+
+FlexMate::Log.puts(c.log)
+
+a = CompletionsList.new(c)
+
+m = a.list_d2
+if m.size > 0
+	FlexMate.complete(m,p[:filter],c.exit_message)
+	exit
+end
+
+FlexMate.tooltip c.exit_message || 'No completions.'
