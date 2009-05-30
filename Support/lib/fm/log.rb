bc0d9bae73f2702c24472587f91d043a123c5a1a
simongregory <simon@helvector.org>
2009-05-30 20:49:20 +0100

    • No longer use a constant for log file path.

--- Support/lib/fm/log.rb
+++ Support/lib/fm/log.rb
@@ -3,41 +3,45 @@
 
 module FlexMate
 
-	module Log
+  module Log
 
-		class << self
+    class << self
 
-			LOG_FILE = "#{e_sh ENV['HOME']}/Library/Logs/TextMate ActionScript 3.log"
+      def log_file
+        require ENV['TM_SUPPORT_PATH'] + '/lib/escape'
+        e_sh "#{ ENV['HOME']}/Library/Logs/TextMate ActionScript 3.log"
+      end
 
-			# Initilialise/clear the log.
-			#
-			def init
-				f = File.open(LOG_FILE, "w")
-				f.close
-			end
+      # Initilialise/clear the log.
+      #
+      def init
+        f = File.open(log_file, "w")
+        f.close
+      end
 
-			# Appends the given text to the ActionScript 3 Bundle log file.
-			#
-			def puts(text)
+      # Appends the given text to the ActionScript 3 Bundle log file.
+      #
+      def puts(text)
 
-				init unless File.exist?(LOG_FILE)
+        init unless File.exist?(log_file)
 
-				f = File.open(LOG_FILE, "a")
-				f.puts Time.now.strftime("\n[%m/%d/%Y %H:%M:%S]") + " TextMate::ActionScript 3.tmbundle"
-				f.puts text
-				f.flush
-				f.close
+        f = File.open(log_file, "a")
+        f.puts Time.now.strftime("\n[%m/%d/%Y %H:%M:%S]") + " TextMate::ActionScript 3.tmbundle"
+        f.puts text
+        f.flush
+        f.close
 
-			end
+      end
 
-		end
-		
-	end
+    end
+
+  end
 
 end
 
 if __FILE__ == $0
 
-	FlexMate::Log.puts("Test Text")
+  FlexMate::Log.puts("Test Text")
+  `open -a TextMate #{FlexMate::Log.log_file}`
 
 end
