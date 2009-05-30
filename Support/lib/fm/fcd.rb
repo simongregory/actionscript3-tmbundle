ac0f502ad29b75fae66c6d9472f6a08d830acac2
simongregory <simon@helvector.org>
2009-05-30 20:26:32 +0100

    • Adding tests for flex config dictionary lookup.

--- Support/lib/fm/fcd.rb
+++ Support/lib/fm/fcd.rb
@@ -10,24 +10,46 @@ class FlexConfigDoc
     d = File.new( p )
     @doc = REXML::Document.new d
   end
-  
+
   def find(scope)
     scope = scope.split(" ").pop().sub('meta.tag.xml.flex-config.','').gsub('.','\.')
     rgx = / #{scope}: /
     REXML::XPath.match(@doc, '//comment()').each { |e|
-      c = e.to_s      
-      return c.sub(rgx,'') if c =~ rgx      
+      c = e.to_s
+      return c.sub(rgx,'') if c =~ rgx
     }
     return 'No documentation found.'
   end
 end
 
 if __FILE__ == $0
-  
-  fcd = FlexConfigDoc.new
-  puts fcd.find('meta.tag.xml.flex-config.compiler.warn-number-from-string-changes')
-  puts fcd.find('meta.tag.xml.flex-config.compiler.defaults-css-files')
-  puts fcd.find('meta.tag.xml.flex-config.compiler.external-library-path')
-  puts fcd.find('meta.tag.xml.flex-config.frames.frame')
-  
+
+  require "test/unit"
+
+  class FlexConfigDocTest < Test::Unit::TestCase
+
+    def test_find
+      
+      checks = [
+        { :node => 'meta.tag.xml.flex-config.compiler.warn-number-from-string-changes',
+          :help => "In ActionScript 3.0, white space is ignored and '' returns 0. Number() returns NaN in ActionScript 2.0 when the parameter is '' or contains white space."
+        },
+        { :node => 'meta.tag.xml.flex-config.compiler.defaults-css-files',
+          :help => 'No documentation found.'
+        },
+        { :node => 'meta.tag.xml.flex-config.compiler.external-library-path',
+          :help => 'list of SWC files or directories to compile against but to omit from linking'
+        },
+        { :node => 'meta.tag.xml.flex-config.frames.frame',
+          :help => 'A SWF frame label with a sequence of classnames that will be linked onto the frame.'
+        }
+      ]
+
+      fcd = FlexConfigDoc.new
+      checks.each { |e| assert_equal(e[:help], fcd.find(e[:node])) }
+
+    end
+
+  end
+
 end
