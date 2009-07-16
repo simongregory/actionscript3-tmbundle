ec930f6d5f80626c0257cf017fd4c7bd43ffea2b
Simon Gregory <simon@helvector.org>
2009-07-16 16:15:49 +0100

    • Wrapped an exception round the code at the top level to aid debugging.

--- Support/bin/complete.rb
+++ Support/bin/complete.rb
@@ -3,32 +3,55 @@
 
 require ENV['TM_BUNDLE_SUPPORT'] + '/lib/c_env'
 
-FlexMate.complete_by_scope if ENV['TM_SCOPE'] =~ /support\.function\.(top-level|flash|fl|mx|global)\.actionscript\.3/
+begin
 
-FlexMate.opt_in_to_completions
+	FlexMate.complete_by_scope if ENV['TM_SCOPE'] =~ /support\.function\.(top-level|flash|fl|mx|global)\.actionscript\.3/
 
-p = PropertyInspector.capture
-TextMate.exit_show_tool_tip("No property found.") if p[:ref] == nil
+	FlexMate.opt_in_to_completions
 
-c = ClassParser.new
+	p = PropertyInspector.capture
+	TextMate.exit_show_tool_tip("No property found.") if p[:ref] == nil
 
-#cp = ConfigParser.new(true)
-#cp.src_paths.each { |p| puts p }  
+	c = ClassParser.new
 
-if p[:is_static]
-	c.load_statics(STDIN.read.strip,p[:ref])
-else
-	c.load(STDIN.read.strip,p[:ref])
+	#cp = ConfigParser.new(true)
+	#cp.src_paths.each { |p| puts p }  
+
+	if p[:is_static]
+		c.load_statics(STDIN.read.strip,p[:ref])
+	else
+		c.load(STDIN.read.strip,p[:ref])
+	end
+
+	FlexMate::Log.puts(c.log)
+
+	a = CompletionsList.new(c)
+
+	m = a.list_d2
+	if m.size > 0
+		FlexMate.complete(m,p[:filter],c.exit_message)
+		#exit
+	else
+		FlexMate.tooltip c.exit_message || 'No completions.'
+	end
+
+rescue Exception => e
+	
+	require ENV['TM_SUPPORT_PATH'] + '/lib/tm/htmloutput'
+	
+  html_out = TextMate::HTMLOutput.show(
+    :title => "AutoCompletion Error",
+    :sub_title => "ActionScript 3"
+  ) do |io|
+    io << <<-HTML		
+			<h2>Exception</h2>
+      <pre>#{e}</pre>
+			<h2>Location</h2>
+			<pre>#{e.backtrace.join("\n")}</pre>
+    HTML
+  end
+	
+  TextMate.exit_show_html(html_out)
+
 end
 
-FlexMate::Log.puts(c.log)
-
-a = CompletionsList.new(c)
-
-m = a.list_d2
-if m.size > 0
-	FlexMate.complete(m,p[:filter],c.exit_message)
-	exit
-end
-
-FlexMate.tooltip c.exit_message || 'No completions.'
