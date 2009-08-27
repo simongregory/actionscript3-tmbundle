b690b3755b4fc07d8149a010979af9efe492615d
Simon Gregory <simon@helvector.org>
2009-08-27 10:10:29 +0100

    • Refactored build, run and associated compilers.
    Both fcsh and mxmlc now have associated compiler classes and use an instance of settings when determining what to compile. Where possible we take some educated guesses to work out what TM_FLEX_FILE_SPECS and TM_FLEX_OUTPUT should be - based off the first file we find in the project root or src directory.

--- Support/bin/build.rb
+++ Support/bin/build.rb
@@ -11,74 +11,31 @@ require File.expand_path(File.dirname(__FILE__)) + '/../lib/add_lib'
 require 'fm/flex_mate'
 require 'fm/sdk'
 require 'fm/compiler'
+require 'fm/settings'
 require 'as3/source_tools'
 
-if ENV['TM_PROJECT_DIRECTORY'] && ENV['TM_FLEX_USE_FCSH']
-
-  require ENV['TM_SUPPORT_PATH'] + '/lib/web_preview'
-  require ENV['TM_SUPPORT_PATH'] + '/lib/tm/htmloutput'
-
-  # Start by trying to add the Flex SDK bin to $PATH then testing for fcsh.
-  FlexMate::SDK.add_flex_bin_to_path
-
-  TextMate.require_cmd "fcsh"
-
-  bp = ENV['TM_PROJECT_DIRECTORY']
-
-  # Simple euristic: take the first *.as|mxml file in the root folder
-  # and use it if no TM_FLEX_FILE_SPECS is set - from Davide 'Folletto' Casali 
-  if !ENV['TM_FLEX_FILE_SPECS']
-
-    src_dir = ENV['TM_PROJECT_DIRECTORY']
-    src_prefix = ''
-    if File.exist?(ENV['TM_PROJECT_DIRECTORY']+'/src')
-      src_dir = ENV['TM_PROJECT_DIRECTORY']+'/src'
-      src_prefix = 'src/'
-    end
-    
-    Dir.chdir(src_dir)
-    Dir['*.as','*.mxml'].sort.each do |name|	
-  		
-  		ENV['TM_FLEX_FILE_SPECS'] = src_prefix+name #File.expand_path(name) # full path
-  		
-  		#TODO: Link to usual src dirs and improve sub with a regexp that
-      #matches src backwards from the end of line.
-      if File.exist?( ENV['TM_PROJECT_DIRECTORY'] + '/bin' )
-        ENV['TM_FLEX_OUTPUT'] = 'bin/'+name.sub(/\.(as|mxml)$/, '.swf')
-      else
-        ENV['TM_FLEX_OUTPUT'] = name.sub(/\.(as|mxml)$/, '.swf')
-      end
-      
-  		break
-  	end
-  end
-  
-  #Check for custom build files and execute them where they exist.
-  custom = "#{bp}/#{ENV['TM_FLEX_BUILD_FILE']}"
-  if File.file?(custom) && File.executable?(custom)    
+#Check for custom build files and execute them where they exist.
+custom = "#{ENV['TM_PROJECT_DIRECTORY']}/#{ENV['TM_FLEX_BUILD_FILE']}"
+if File.file?(custom)
+  if File.executable?(custom)
     TextMate::Process.run(custom) do |str|
       STDOUT << str
     end
-    TextMate.exit_show_html
+  else
+    puts "WARNING: #{custom} not executable."  
   end
+  TextMate.exit_show_html
+end
   
-  s = { :files => ['TM_FLEX_FILE_SPECS'], 
-        :evars => ['TM_FLEX_OUTPUT'],
-        :base_path => bp }
-
-  FlexMate.required_settings(s)
-
-  file_specs  = bp + '/' + ENV['TM_FLEX_FILE_SPECS']
-  flex_output = bp + '/' + ENV['TM_FLEX_OUTPUT']
-  fcsh       = e_sh(ENV['TM_FLEX_PATH'] + '/bin/fcsh')
-
-  FlexMate.check_valid_paths([file_specs,flex_output,fcsh])
-
-  mxmlc_args="mxmlc -o=#{flex_output} -file-specs=#{file_specs}"
+if ENV['TM_PROJECT_DIRECTORY'] && ENV['TM_FLEX_USE_FCSH']
   
-  `osascript -e 'tell application "Terminal" to activate'` unless ENV['TM_FLEX_BACKGROUND_TERMINAL']
-  `#{e_sh ENV['TM_BUNDLE_SUPPORT']}/lib/fcsh_terminal \"#{fcsh}\" \"#{mxmlc_args}\" >/dev/null;`
+  #Requires are needed by FlexMate.required_settings + check_valid_paths
+  require ENV['TM_SUPPORT_PATH'] + '/lib/web_preview'
+  require ENV['TM_SUPPORT_PATH'] + '/lib/tm/htmloutput'
 
+  c = FlexMate::FcshCompiler.new
+  c.build
+  
   TextMate.exit_discard
   
 else
